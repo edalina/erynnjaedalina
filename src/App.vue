9dde27fc6f52d90f0580d9d77b38f9cdef620923
ejdalina <ej.dalina@tooltwist.com>
2020-05-16 21:11:35 +0800

    Beta Website

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,33 @@
-<template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+<template lang="pug">
+  #app
+    //- #nav
+    //-   b-navbar(:transparent='true')
+    //-     template(slot='brand')
+    //-       b-navbar-item(tag='router-link' :to='{ path: "/" }')
+    //-         img(src="@/assets/images/self.jpg")
+    //-     template(slot='start')
+    //-       b-navbar-item(href="#") Home
+    //-       b-navbar-item(href="#") About Me
+    router-view
 </template>
 
 <style lang="scss">
+@import url('https://use.fontawesome.com/releases/v5.2.0/css/all.css');
+
+@font-face {
+  font-family: 'Roboto';
+  src: url('~@/assets/fonts/Roboto/Roboto-Regular.ttf');
+}
+
+a, a:hover, a:active {
+  color: #363636 !important;
+}
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: Roboto, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,179 @@
-<template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+<template lang="pug">
+  .home
+    .banner.position-relative
+      parallax(:speed-factor="0.3" :sectionHeight='100')
+        img.parallax-image(src='@/assets/images/wallpaper-lake.jpg')
+      .img-holder.position-absolute.black-20-overlay
+        img(src='@/assets/images/self.jpg')
+        .main-title Erynn Jae Dalina
+    .portfolio-section.section
+      section.hero.width-75
+        .hero-body
+          .container
+            h1.title Portfolio
+            .portfolios-wrapper
+              .portfolio
+                .icon
+                  i.fab.fa-node
+                .head-title
+                  | NodeJS
+                .description
+                  | 4+ years experience
+              .portfolio
+                .icon
+                  i.fab.fa-java
+                .head-title
+                  | Java EE
+                .description
+                  | 4+ years experience
+              .portfolio
+                .icon
+                  i.fas.fa-code
+                .head-title
+                  | HTML &amp; CSS
+                .description
+                  | 7+ years experience
+            .portfolios-wrapper
+              .portfolio
+                .icon
+                  i.fab.fa-vuejs
+                .head-title
+                  | VueJS
+                .description
+                  | 3+ years experience
+              .portfolio
+                .icon
+                  i.fas.fa-database
+                .head-title
+                  | SQL
+                .description
+                  | 7+ years experience
+              .portfolio
+                .icon
+                  i.fab.fa-react
+                .head-title
+                  | React Native
+                .description
+                  | 6 months experience
+    .about-section.section.position-relative
+      .black-20-overlay.position-absolute
+        section.hero
+          .hero-body
+            .container
+              //- h1.title About
+              h2.title Simple is better.
+              h3 Father. Husband. Son. Developer.
+    footer.footer
+      .content.has-text-centered
+        div Erynn Jae Dalina &copy; 2020
+        div
+          a.social-icons(href='https://www.facebook.com/erynnjaedalina' target='_blank')
+            i.fab.fa-facebook
+          a.social-icons(href='mailto:yjaedalina@gmail.com')
+            i.fas.fa-at
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import HomeComponent from '@/components/HomeComponent.vue'
+import Parallax from 'vue-parallaxy'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    HomeComponent,
+    Parallax
+  },
+  data () {
+    return {
+    }
   }
 }
 </script>
+
+<style lang="scss" scoped>
+  .banner {
+    & .img-holder {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      flex-direction: column;
+      & img {
+        max-height: 250px;
+        height: 80vh;
+        border-radius: 100%;
+        opacity: 0.9;
+      }
+      & .main-title {
+        // -webkit-text-stroke-width: 1px;
+        // -webkit-text-stroke-color: #CCC;
+        color: #EFEFEF;
+        font-size: 2rem;
+        text-align: center;
+      }
+    }
+  }
+  .section {
+    height: 100vh;
+    padding: 0 !important;
+    margin: 0 !important;
+    display: flex;
+    align-content: center;
+    justify-content: center;
+    &:nth-child(odd) {
+      background-color: #EEE;
+    }
+  }
+  .portfolios-wrapper {
+    display: flex;
+    justify-content: space-between;
+    margin-bottom: 50px;
+    & .portfolio {
+      flex: 1;
+      & .icon {
+        font-size: 4em;
+        margin-bottom: 50px;
+      }
+      & .head-title {
+        font-size: 1.1em;
+        font-weight: bold;
+        margin-bottom: 10px;
+      }
+      & .description {
+
+      }
+    }
+  }
+  .about-section {
+    background: url('~@/assets/images/couple-baguio-wacky.jpg');
+    & .position-absolute {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      flex-direction: column;
+    }
+    & * {
+      color: #EFEFEF !important;
+    }
+  }
+  .social-icons {
+    font-size: 1.5em;
+    margin: 10px;
+  }
+  .width-75 {
+    width: 75% !important;
+  }
+  .position-relative {
+    position: relative;
+  }
+  .position-absolute {
+    position: absolute;
+    top: 0;
+    left: 0;
+    bottom: 0;
+    right: 0;
+  }
+  .black-20-overlay {
+    background: rgba(0, 0, 0, 0.5);
+  }
+</style>
