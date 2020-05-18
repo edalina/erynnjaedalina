d598beef808e552e18800cf9e1aacf820e56df0f
ejdalina <ej.dalina@tooltwist.com>
2020-05-18 17:52:51 +0800

    Adding favicons and GTM

--- src/main.js
+++ src/main.js
@@ -3,8 +3,15 @@ import App from './App.vue'
 import router from './router'
 import Buefy from 'buefy'
 import 'buefy/dist/buefy.css'
+import VueGtm from 'vue-gtm'
 
 Vue.use(Buefy)
+Vue.use(VueGtm, {
+  id: 'GTM-TPRB93T',
+  enabled: true,
+  loadScript: true,
+  vueRouter: router
+})
 Vue.config.productionTip = false
 
 new Vue({
